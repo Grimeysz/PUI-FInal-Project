f99d8c5d8ea69194c359c2b4a9468f63f2c657f1
Grimeysz <32318053+Grimeysz@users.noreply.github.com>
2021-12-04 21:22:39 -0500

    added images and animations

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  //$("#img2").stop(true, true).delay(400).animate({ marginLeft: "50vw" });
+
+  $("#neverjudge").stop(true, true).delay(400).animate({
+    //left: "20vw",
+
+    opacity: "1",
+    fontSize: "7rem"
+  });
+
+  //$("#img3").stop(true, true).delay(800).animate({ marginLeft: "40vw" });
+
+  $("#abook").stop(true, true).delay(800).animate({
+    //left: "5vw",
+
+    opacity: "1",
+    fontSize: "7rem"
+  });
+
+  $("#byitscover").stop(true, true).delay(1200).animate({
+    //left: "35vw",
+
+    opacity: "1",
+    fontSize: "7rem"
+  });
+
+  // $(window).scroll(function () {
+  //   var winHeight = $(this).height();
+  //   var scrollTop = $(this).scrollTop();
+  //   console.log(scrollTop);
+  //   var elemHeight = $(".row").height();
+  //   var elementTop = $(".row").position().top;
+  //   if (scrollTop > 1000) {
+  //     $("#neverjudge").animate({
+  //       left: "25vw",
+  //       position: "relative",
+  //       opacity: "1"
+  //     });
+  //     $("#img2").animate({ marginLeft: "40vw", position: "relative" });
+  //     console.log("true");
+  //   }
+  //   if (scrollTop > 2000) {
+  //     $("#abook").animate({
+  //       left: "5vw",
+  //       position: "relative",
+  //       opacity: "1"
+  //     });
+  //     $("#img3").animate({ marginLeft: "40vw", position: "relative" });
+  //     console.log("true");
+  //   }
+  //   if (scrollTop > 3000) {
+  //     $("#byitscover").animate({
+  //       left: "35vw",
+  //       position: "relative",
+  //       opacity: "1"
+  //     });
+  //     console.log("true");
+  //   }
+  // });
+});
